b635cbeae3101ed1ea01b23b5aefaf946daf79a3
Thomas Francart <thomas.francart@sparna.fr>
2026-03-02 08:51:19 +0100

    Build site and publish at lun. 02 mars 2026 08:51:19 CET

--- site/publish.sh
+++ site/publish.sh
@@ -2,8 +2,9 @@ export FINAL_FOLDER="dist"
 
 cd site
 
+npm run clean
 npm run build
 
 git add --all
 git commit -m "Build site and publish at $(date)"
-git push origin master
+git push origin main
