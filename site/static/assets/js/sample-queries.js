a59321ee171245d243f4e148f1370a03f9990533
Marie Muller <marie.muller@sparna.fr>
2026-03-06 13:02:00 +0100

    màj formulaire + sample queries

--- site/static/assets/js/sample-queries.js
+++ site/static/assets/js/sample-queries.js
@@ -1,219 +1,366 @@
 var queries = [
   {
-    label_en: "Artworks displayed in Italian museums",
-    label_fr: "Les œuvres exposées dans des musées italiens",
+    label_en: "Procedures for assault and battery",
+    label_fr: "Procédures pour coups et blessures",
     query: {
-      distinct: true,
-      variables: [
+  "distinct": true,
+  "variables": [
+    {
+      "termType": "Variable",
+      "value": "Procedure"
+    }
+  ],
+  "order": null,
+  "branches": [
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_resultsOrResultedFrom",
+        "o": "Fait",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+        "criterias": []
+      },
+      "children": [
         {
-          termType: "Variable",
-          value: "Artwork_1",
-        },
-        {
-          termType: "Variable",
-          value: "Museum_2",
-        },
-        {
-          termType: "Variable",
-          value: "Country_4",
-        },
-      ],
-      order: null,
-      branches: [
-        {
-          line: {
-            s: "Artwork_1",
-            p: "https://data.mydomain.com/ontologies/sparnatural-config/Artwork_displayedAt",
-            o: "Museum_2",
-            sType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Artwork",
-            oType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Museum",
-            values: [],
-          },
-          children: [
-            {
-              line: {
-                s: "Museum_2",
-                p: "https://data.mydomain.com/ontologies/sparnatural-config/Museum_country",
-                o: "Country_4",
-                sType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Museum",
-                oType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Country",
-                values: [
-                  {
-                    label: "Italy",
-                    rdfTerm: {
-                      type: "uri",
-                      value: "http://dbpedia.org/resource/Italy",
-                    },
-                  },
-                ],
-              },
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
-    label_en: "Museums where Van Goghs artworks are displayed",
-    label_fr: "Les musées qui exposent les oeuvres de Vincent Van Gogh",
-    query: {
-      distinct: true,
-      variables: [
-        {
-          termType: "Variable",
-          value: "Museum_1",
-        },
-        {
-          termType: "Variable",
-          value: "Artwork_2",
-        },
-        {
-          termType: "Variable",
-          value: "Person_4",
-        },
-      ],
-      order: null,
-      branches: [
-        {
-          line: {
-            s: "Museum_1",
-            p: "https://data.mydomain.com/ontologies/sparnatural-config/Museum_displays",
-            o: "Artwork_2",
-            sType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Museum",
-            oType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Artwork",
-            values: [],
-          },
-          children: [
-            {
-              line: {
-                s: "Artwork_2",
-                p: "https://data.mydomain.com/ontologies/sparnatural-config/Artwork_author",
-                o: "Person_4",
-                sType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Artwork",
-                oType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Person",
-                values: [
-                  {
-                    label: "Vincent van Gogh",
-                    rdfTerm: {
-                      type: "uri",
-                      value: "http://dbpedia.org/resource/Vincent_van_Gogh",
-                    },
-                  },
-                ],
-              },
-              children: [],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
-    label_en:
-      "19th Century French women artists, movements, artworks & museums",
-    label_fr:
-      "Les artistes françaises du XIXe, mouvements, œuvres & lieux d'exposition",
-    query: {
-      distinct: true,
-      variables: [
-        {
-          termType: "Variable",
-          value: "Person_1",
-        },
-        {
-          termType: "Variable",
-          value: "Category_2",
-        },
-        {
-          termType: "Variable",
-          value: "Movement_6",
-        },
-        {
-          termType: "Variable",
-          value: "Artwork_8",
-        },
-        {
-          termType: "Variable",
-          value: "Museum_10",
-        },
-      ],
-      order: null,
-      branches: [
-        {
-          line: {
-            s: "Person_1",
-            p: "https://data.mydomain.com/ontologies/sparnatural-config/Person_classifiedIn",
-            o: "Category_2",
-            sType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Person",
-            oType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Category",
-            values: [
+          "line": {
+            "s": "Fait",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait_hasEventType",
+            "o": "EventType",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#EventType",
+            "criterias": [
               {
-                label: "19th-century French women artists",
-                rdfTerm: {
-                  type: "uri",
-                  value:
-                    "http://dbpedia.org/resource/Category:19th-century_French_women_artists",
-                },
-              },
-            ],
-          },
-          children: [],
-        },
-        {
-          line: {
-            s: "Person_1",
-            p: "https://data.mydomain.com/ontologies/sparnatural-config/Person_movement",
-            o: "Movement_6",
-            sType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Person",
-            oType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Movement",
-            values: [],
-          },
-          children: [],
-          optional: true,
-        },
-        {
-          line: {
-            s: "Person_1",
-            p: "https://data.mydomain.com/ontologies/sparnatural-config/Person_created",
-            o: "Artwork_8",
-            sType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Person",
-            oType:
-              "https://data.mydomain.com/ontologies/sparnatural-config/Artwork",
-            values: [],
-          },
-          children: [
-            {
-              line: {
-                s: "Artwork_8",
-                p: "https://data.mydomain.com/ontologies/sparnatural-config/Artwork_displayedAt",
-                o: "Museum_10",
-                sType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Artwork",
-                oType:
-                  "https://data.mydomain.com/ontologies/sparnatural-config/Museum",
-                values: [],
-              },
-              children: [],
-              optional: true,
-            },
-          ],
-          optional: true,
-        },
-      ],
-    },
+                "label": "coup et blessure",
+                "criteria": {
+                  "rdfTerm": {
+                    "type": "uri",
+                    "value": "https://data.archives.haute-garonne.fr/type/facou"
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ],
+  "limit": 1000
+}
   },
-];
+  {
+    label_en: "Places where the facts were investigated by the seneschal of Toulouse",
+    label_fr: "Lieux des faits instruits par la sénéchaussée de Toulouse",
+    query: {
+  "distinct": true,
+  "variables": [
+    {
+      "termType": "Variable",
+      "value": "Lieu"
+    }
+  ],
+  "order": null,
+  "branches": [
+    {
+      "line": {
+        "s": "Lieu",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Lieu_inverse_hasOrHadLocation",
+        "o": "Fait",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Lieu",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "Fait",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait_inverse_resultsOrResultedFrom",
+            "o": "Procedure",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+            "criterias": []
+          },
+          "children": [
+            {
+              "line": {
+                "s": "Procedure",
+                "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_hasOrHadSubevent",
+                "o": "Instruction",
+                "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+                "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+                "criterias": []
+              },
+              "children": [
+                {
+                  "line": {
+                    "s": "Instruction",
+                    "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction_isOrWasPerformedBy",
+                    "o": "Juridiction",
+                    "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+                    "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction",
+                    "criterias": [
+                      {
+                        "label": "France. Sénéchaussée de Toulouse",
+                        "criteria": {
+                          "rdfTerm": {
+                            "type": "uri",
+                            "value": "https://data.archives.haute-garonne.fr/agent/51df72a4-01d7-11f1-bcc0-94e70b70a1ec"
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "limit": 1000
+}
+
+  },
+  {
+    label_en: "Changes in referrals to seigneurial courts over the years",
+    label_fr: "Évolution des saisines de juridictions seigneuriales au cours des années",
+    query: {
+  "distinct": true,
+  "variables": [
+    {
+      "expression": {
+        "type": "aggregate",
+        "aggregation": "count",
+        "distinct": false,
+        "expression": {
+          "termType": "Variable",
+          "value": "Procedure"
+        }
+      },
+      "variable": {
+        "termType": "Variable",
+        "value": "Procedure_count"
+      }
+    },
+    {
+      "termType": "Variable",
+      "value": "DateLiteral"
+    }
+  ],
+  "order": null,
+  "branches": [
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_hasOrHadSubevent",
+        "o": "Instruction",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "Instruction",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction_isOrWasPerformedBy",
+            "o": "Juridiction",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction",
+            "criterias": []
+          },
+          "children": [
+            {
+              "line": {
+                "s": "Juridiction",
+                "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction_hasOrHadLegalStatus",
+                "o": "LegalStatus",
+                "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction",
+                "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#LegalStatus",
+                "criterias": [
+                  {
+                    "label": "justice seigneuriale",
+                    "criteria": {
+                      "rdfTerm": {
+                        "type": "uri",
+                        "value": "https://data.archives.haute-garonne.fr/type/jusei"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_hasBeginningDate",
+        "o": "Date",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Date",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "Date",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Date_normalizedDateValue",
+            "o": "DateLiteral",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Date",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#DateLiteral",
+            "criterias": []
+          }
+        }
+      ]
+    }
+  ],
+  "limit": 1000
+}
+
+  },
+  {
+    label_en: "Facts that bring women before forestry courts",
+    label_fr: "Faits qui amènent des femmes devant des juridictions forestières",
+    query: {
+  "distinct": true,
+  "variables": [
+    {
+      "termType": "Variable",
+      "value": "Procedure"
+    },
+    {
+      "expression": {
+        "type": "aggregate",
+        "aggregation": "group_concat",
+        "distinct": false,
+        "expression": {
+          "termType": "Variable",
+          "value": "Libelle"
+        }
+      },
+      "variable": {
+        "termType": "Variable",
+        "value": "Libelle_group_concat"
+      }
+    }
+  ],
+  "order": null,
+  "branches": [
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_hasOrHadParticipant",
+        "o": "PersonnePhysique",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#PersonnePhysique",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "PersonnePhysique",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#PersonnePhysique_hasOrHadDemographicGroup",
+            "o": "DemographicGroup",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#PersonnePhysique",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#DemographicGroup",
+            "criterias": [
+              {
+                "label": "Femme",
+                "criteria": {
+                  "rdfTerm": {
+                    "type": "uri",
+                    "value": "https://data.archives.haute-garonne.fr/type/sxfem"
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    },
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_hasOrHadSubevent",
+        "o": "Instruction",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "Instruction",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction_isOrWasPerformedBy",
+            "o": "Juridiction",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Instruction",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction",
+            "criterias": []
+          },
+          "children": [
+            {
+              "line": {
+                "s": "Juridiction",
+                "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction_hasOrHadLegalStatus",
+                "o": "LegalStatus",
+                "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Juridiction",
+                "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#LegalStatus",
+                "criterias": [
+                  {
+                    "label": "eaux et forêts",
+                    "criteria": {
+                      "rdfTerm": {
+                        "type": "uri",
+                        "value": "https://data.archives.haute-garonne.fr/type/jueau"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "line": {
+        "s": "Procedure",
+        "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure_resultsOrResultedFrom",
+        "o": "Fait",
+        "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Procedure",
+        "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+        "criterias": []
+      },
+      "children": [
+        {
+          "line": {
+            "s": "Fait",
+            "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait_hasEventType",
+            "o": "EventType",
+            "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Fait",
+            "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#EventType",
+            "criterias": []
+          },
+          "children": [
+            {
+              "line": {
+                "s": "EventType",
+                "p": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#EventType_prefLabel",
+                "o": "Libelle",
+                "sType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#EventType",
+                "oType": "https://data.archives.haute-garonne.fr/modeles/sacsaproces#Libelle",
+                "criterias": []
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "limit": 1000
+}
+
+]
